e84b8c3401a50df9a51e20efabf1482528d75074
devonjkohler <devonjkohler@gmail.com>
2022-05-09 12:25:49 -0400

    parrallel for lo

--- examples/Testing_scripts/gen_lo_samples.py
+++ examples/Testing_scripts/gen_lo_samples.py
@@ -5,12 +5,14 @@ import pickle
 import numpy as np
 import pandas as pd
 import torch
+import sbi.utils as utils
+import multiprocessing as mp
 
 ## Define sim
-def lo_simulation(rates):
+def lo_simulation(i, rates):
 
-    sbml_model = Model(sbml_filename=r"../LacOperon_stochastic.xml", sbml_warnings=False)
-    # sbml_model = Model(sbml_filename=r"LacOperon_stochastic.xml", sbml_warnings=False)
+    sbml_model = Model(sbml_filename=r"/home/kohler.d/code_output/biosim_project/sbi/examples/LacOperon_stochastic.xml", sbml_warnings=False)
+    # sbml_model = Model(sbml_filename=r"../LacOperon_stochastic.xml", sbml_warnings=False)
 
     ## Update rates based on sample from prior
     rate_names = [
@@ -38,22 +40,31 @@ def lo_simulation(rates):
 
     # trace = torch.flatten(sim_path)
 
-    return sim_path.T
+    return (i, sim_path.T)
 
 def main():
+    pool = mp.Pool(mp.cpu_count())
+    obs_list = list()
+    def collect_result(result):
+        obs_list.append(result)
 
     prior = utils.BoxUniform(
         low=torch.tensor([20.,  120000.]),
         high=torch.tensor([40.,  180000.]),)
-
-    obs_list = list()
+    print("entering for loop")
     labels = list()
-    for i in range(100):
+    for i in range(1000):
         prior_sample = prior.sample()
         labels.append(prior_sample)
-        obs_list.append(lo_simulation(prior_sample))
+        pool.apply_async(lo_simulation, args=(i, prior_sample), callback=collect_result)
 
-    x = torch.stack(obs_list,axis=0)
+    pool.close()
+    pool.join()
+    print("obs Gen")
+    obs_list.sort(key=lambda x: x[0])
+    obs_list_final = [r for i, r in obs_list]
+    print("obs sorted")
+    x = torch.stack(obs_list_final,axis=0)
     y = torch.stack(labels,axis=0)
 
     ## Save observations
