6ed4c4da582967a989a54d08487f3f7eb9696f44
devonjkohler <devonjkohler@gmail.com>
2022-05-11 12:24:09 -0400

    LO sim

---
+++ examples/Testing_scripts/lo_cnn.py
@@ -0,0 +1,209 @@
+import pickle
+
+## CNN functions
+import torch
+from torch.nn import Conv2d, ReLU, Linear, Sequential, MaxPool2d, AvgPool2d, Dropout, Module, CrossEntropyLoss
+from torch.autograd import Variable
+from torch.optim import Adam
+from torch.utils.data import Dataset, DataLoader
+
+from sklearn.model_selection import train_test_split
+
+class Net(Module):
+    def __init__(self):
+        super(Net, self).__init__()
+
+        self.cnn_layers = Sequential(
+
+            # Defining a 2D convolution layer
+            Conv2d(1, 64, kernel_size=(3, 3), padding=(1, 0)),
+            ReLU(inplace=True),
+            Conv2d(64, 64, kernel_size=(3, 3), padding=(1, 0)),
+            ReLU(inplace=True),
+            MaxPool2d((1, 5)),
+            Conv2d(64, 128, kernel_size=(3, 3), padding=(1, 0)),
+            ReLU(inplace=True),
+            MaxPool2d((1, 5)),
+            Conv2d(128, 128, kernel_size=(3, 3), padding=(1, 0)),
+            ReLU(inplace=True),
+            MaxPool2d((1, 5)),
+            Conv2d(128, 128, kernel_size=(3, 3), padding=(1, 0)),
+            ReLU(inplace=True),
+            AvgPool2d(kernel_size=(1, 5))
+        )
+
+        self.linear_layers = Sequential(
+            Linear(1408, 720)
+        )
+
+        self.middle_linear_layers = Sequential(
+            Linear(720, 720)
+        )
+
+        self.relu = Sequential(
+            ReLU(inplace=True),
+            Dropout(p=.15)
+        )
+
+        self.output = Sequential(
+            Linear(720, 2),
+        )
+
+    # Defining the forward pass
+    def forward(self, x):
+        x = self.cnn_layers(x)
+        x = self.relu(x)
+        # x = x.view(-1)
+        x = x.view(x.size(0), -1)
+        x = self.linear_layers(x)
+        x = self.relu(x)
+        x = self.middle_linear_layers(x)
+        x = self.relu(x)
+        x = self.middle_linear_layers(x)
+        x = self.relu(x)
+        x = self.middle_linear_layers(x)
+        x = self.relu(x)
+
+        x = self.output(x)
+
+        return x
+
+## Define class to batch data
+class TraceDataSet(Dataset):
+
+  def __init__(self, X, Y):
+    self.X = X
+    self.Y = Y
+    if len(self.X) != len(self.Y):
+      raise Exception("The length of X does not match the length of Y")
+
+  def __len__(self):
+    return len(self.X)
+
+  def __getitem__(self, index):
+    # note that this isn't randomly selecting. It's a simple get a single item that represents an x and y
+    _x = self.X[index]
+    _y = self.Y[index]
+
+    return _x, _y
+
+## CNN train function
+def train(epoch, batch_idx, model, train_x, train_y, val_x, val_y,
+          optimizer, criterion, train_losses, val_losses):
+    model.train()
+    tr_loss = 0
+    # getting the training set
+    x_train, y_train = Variable(train_x), Variable(train_y)
+    # getting the validation set
+    x_val, y_val = Variable(val_x), Variable(val_y)
+    # converting the data into GPU format
+    # if torch.cuda.is_available():
+    #     x_train = x_train.cuda()
+    #     y_train = y_train.cuda()
+    #     x_val = x_val.cuda()
+    #     y_val = y_val.cuda()
+
+    # clearing the Gradients of the model parameters
+    optimizer.zero_grad()
+
+    # prediction for training and validation set
+    output_train = model(x_train)
+    output_val = model(x_val)
+
+    # computing the training and validation loss
+    loss_train = criterion(output_train, y_train)
+    loss_val = criterion(output_val, y_val)
+    train_losses.append(loss_train)
+    val_losses.append(loss_val)
+
+    # computing the updated weights of all the model parameters
+    loss_train.backward()
+    optimizer.step()
+    tr_loss = loss_train.item()
+    # if epoch % 256 == 0:
+        # printing the validation loss
+    print('batch_idx : ', batch_idx, '\t', 'val loss :', loss_val)
+    print('batch_idx : ', batch_idx, '\t', 'train loss :', loss_train)
+
+def main():
+    print("loading data")
+    with open(r'/scratch/kohler.d/code_output/biosim/lac_operon_obs.pickle', 'rb') as handle:
+        x = pickle.load(handle)
+    with open(r'/scratch/kohler.d/code_output/biosim/lac_operon_labels.pickle', 'rb') as handle:
+        y = pickle.load(handle)
+
+    ## Prep data
+    x = x.reshape(6000, 12, 1000)
+    x = x[:, [x for x in range(12) if x != 9], :]
+
+    min_list = list()
+    max_list = list()
+    for i in range(11):
+        col_min = x[:, i].min() - 1
+        min_list.append(col_min)
+        col_max = x[:, i].max()
+        max_list.append(col_max)
+
+        x[:, i] = (x[:, i] - col_min) / (col_max - col_min)
+
+    train_x, val_x, train_y, val_y = train_test_split(x, y, test_size=0.05)
+    train_x = train_x.reshape(int(training_obs*.95), 1, 3, 1000)
+    val_x = val_x.reshape(int(training_obs*.05), 1, 3, 1000)
+
+    # defining the model
+    model = Net()
+    # defining the optimizer
+    optimizer = Adam(model.parameters(), lr=0.001)
+    # defining the loss function
+    criterion = CrossEntropyLoss()
+
+    # defining the number of epochs
+    n_epochs = 30
+    # empty list to store training losses
+    train_losses = []
+    # empty list to store validation losses
+    val_losses = []
+
+    # training the model
+    loader = DataLoader(TraceDataSet(train_x, train_y), batch_size=batch_size, shuffle=True)
+
+    # training the model
+    for epoch in range(n_epochs):
+        print("epoch:{0}".format(str(epoch)))
+        # loader = iter(loader)
+        # for i in range(0, train_x.size()[0], batch_size):
+        for batch_idx, (batch_x, batch_y) in enumerate(loader):
+            # print("batch:{0}".format(str(i)))
+
+            # batch_x, batch_y = loader.next()
+
+            train(epoch, batch_idx, model, batch_x, batch_y, val_x,
+                  val_y, optimizer, criterion, train_losses, val_losses)
+
+        if epoch % 10 == 0:
+            losses = {"train": train_losses, "val": val_losses}
+            state = {
+                'epoch' : epoch,
+                'state_dict': model.state_dict(),
+                'optimizer': optimizer.state_dict(),
+            }
+            with open(r'/scratch/kohler.d/code_output/biosim/cnn_losses_lo.pickle', 'wb') as handle:
+                pickle.dump(losses, handle, protocol=pickle.HIGHEST_PROTOCOL)
+            torch.save(state, r'/scratch/kohler.d/code_output/biosim/cnn_model_lo_temp.pth')
+
+    losses = {"train" : train_losses, "val" : val_losses}
+
+    state = {
+        'state_dict': model.state_dict(),
+        'optimizer': optimizer.state_dict(),
+    }
+
+    print("trying to save cnn losses")
+    with open(r'/scratch/kohler.d/code_output/biosim/cnn_losses_lo.pickle', 'wb') as handle:
+        pickle.dump(losses, handle, protocol=pickle.HIGHEST_PROTOCOL)
+    print("trying to save cnn model")
+    torch.save(state, r'/scratch/kohler.d/code_output/biosim/cnn_model_lo.pth')
+    print("saved")
+
+if __name__ == '__main__':
+    main()
